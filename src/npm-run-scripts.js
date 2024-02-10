3df79dd8671bc76fb4598930f5f4b4e7d05e5a56
Simon Li <orpheus+devel@gmail.com>
2024-02-10 21:30:57 +0000

    Debug network

--- src/npm-run-scripts.js
+++ src/npm-run-scripts.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { spawnSync } = require("child_process");
+const https = require("https");
 
 function run(args) {
   console.log(`Running: ${args.join(" ")}`);
@@ -12,6 +13,37 @@ function run(args) {
     process.exit(r.status);
   }
 }
+
+function getIpAddress() {
+  return new Promise((resolve, reject) => {
+    https
+      .get("https://wtfismyip.com/json", (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+        res.on("end", () => {
+          try {
+            const result = JSON.parse(data);
+            resolve(result);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+}
+
+getIpAddress()
+  .then((result) => {
+    console.log(JSON.stringify(result, null, 2));
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 run(["npm", "install"]);
 process.argv.slice(2).forEach((script) => run(["npm", "run", script]));
